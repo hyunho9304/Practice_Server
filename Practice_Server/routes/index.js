bb6c4d3aeaf25bd0ce150846281664a0eb758f8a
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-07-09 13:06:52 +0900

    COMMIT // duplication var upload

--- Practice_Server/routes/index.js
+++ Practice_Server/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//	하나의 변수에 여러개의 값 업로드 처리
+const duplicationVarUpload = require( './duplicationVarUpload/index' ) ;
+router.use( '/duplicationVarUpload' , duplicationVarUpload ) ;
 
 module.exports = router;
+
+

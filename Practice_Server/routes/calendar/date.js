348b1eb1545aded293121344b85816472041e3a1
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-10-09 15:20:08 +0900

    COMMIT // practice sendmail test

--- Practice_Server/routes/calendar/date.js
+++ Practice_Server/routes/calendar/date.js
@@ -4,6 +4,25 @@ const pool = require('../../config/dbPool');
 const async = require('async');
 const moment = require( 'moment' ) ;
 
+var nodemailer = require('nodemailer');
+
+module.exports.transport = nodemailer.createTransport({  
+    service: 'gmail',
+    auth: {
+        user: 'hyunho9304@gmail.com',
+        pass: 'tkfkd6749'
+    }
+});
+
+module.exports.option = {  
+    from: 'hyunho9304@gmail.com',
+    to: 'hyunho9304@gmail.com',
+    subject: '테스트',
+    html:'<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body><div style="margin : 20px auto; border: 1px solid #cccccc; width:500px;">'+
+    '<div class="title" style="font-size: 30px; text-align: center; margin-top:15px">'+
+    '사업자 등록 인증 메일입니다.</div></div></body></html>'
+};
+
 router.get( '/' , function( req, res ) {
 
 	let maxDate = [ 31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 ] ;
@@ -91,6 +110,49 @@ router.get( '/' , function( req, res ) {
 				} ,
 				message : "successful get currentDay"
 			}) ;
+
+      		// let option = nodemailer.option ;
+      		// let transport = nodemailer.transport ;
+
+      		// transport.sendMail( option , function( err , response ) {
+        // 		if( err ) {
+        //   			transport.close() ;
+        //   			callback( null , "fail" ) ;
+        // 		} else {
+        //   			transport.close() ;
+        //   			callback( null , "successMail" ) ;
+        // 		}
+      		// }) ;
+
+      		var nodemailer = require('nodemailer');
+			var transporter = nodemailer.createTransport( {
+    			service:'gmail',
+    			auth: {
+        			user : 'hyunho9304@gmail.com',
+        			pass : 'tkfkd6749'
+    			}
+			});
+
+			var mailOption = {
+    			from : '박현호 <hyunho9304@gmail.com>',
+    			to : '',
+    			subject : 'nodemailer test',
+    			html:'<h1>HTML 보내기 테스트</h1><p><img src="https://hyunho9304.s3.ap-northeast-2.amazonaws.com/1538040896397.jpg"/></p>'
+			};
+
+			let toMail = "gusgh1492@naver.com"
+
+			mailOption.to = toMail ;
+
+			transporter.sendMail(mailOption, function(err, info) {
+    			if ( err ) {
+        			console.error('Send Mail error : ', err);
+    			}
+    			else {
+        			console.log('Message sent : ', info);
+    			}
+			});
+
 			callback( null , "successful get currentDay" ) ;
 		}
 	] ;

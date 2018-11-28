f92460f9421bd3de3683217e7fa4c6c6f36ab12b
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-11-29 03:42:44 +0900

    COMMIT // public data

--- Practice_Server/routes/publicData/open.js
+++ Practice_Server/routes/publicData/open.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 const fs = require('fs');
 var request = require('request');
 
-router.get('/', function(req, res) {
+router.get('/:powercode/:startdate/:enddate/:ho', function(req, res) {
 
     let task = [
 
@@ -27,16 +27,38 @@ router.get('/', function(req, res) {
         },
 
         function(connection, callback) {
+            const powercode = req.params.powercode;
+            const startdate = req.params.startdate;
+            const enddate = req.params.enddate;
+            const ho = req.params.ho;
 
-            var url = 'http://dataopen.kospo.co.kr/openApi/Conce/AirPollutant';
+            var tmpUrl1 = 'http://dataopen.kospo.co.kr/openApi/Conce/AirPollutant';
+            var tmpUrl2 = 'http://dataopen.kospo.co.kr/openApi/Gene/GenInfoList';
+            var tmpUrl3 = 'http://dataopen.kospo.co.kr/openApi/Conce/ardConcentration' ;
+
+            var url = tmpUrl1;
+
+
+//  tmpUrl1 => 배출농도
             var queryParams = '?' + encodeURIComponent('ServiceKey') + '=22op4iVErXKCKm1jqNWSpzQ3Mo%2FoQYIIOquxrGwyyNSnC86o21TLuPaQGQ%2BH%2BLRT0hsvTo%2BG7UaPsBrMhmRZOg%3D%3D'; /* Service Key*/
             queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('22op4iVErXKCKm1jqNWSpzQ3Mo%2FoQYIIOquxrGwyyNSnC86o21TLuPaQGQ%2BH%2BLRT0hsvTo%2BG7UaPsBrMhmRZOg%3D%3D'); /* 공공데이터포털에서 받은 인증키 */
             queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
-            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('15'); /* 한 페이지 결과 수 */
-            queryParams += '&' + encodeURIComponent('strSdate') + '=' + encodeURIComponent('201701'); /* 조회 시작월 */
-            queryParams += '&' + encodeURIComponent('strEdate') + '=' + encodeURIComponent('201712'); /* 조회 종료월(최대 1년) */
-            queryParams += '&' + encodeURIComponent('strOrgCd') + '=' + encodeURIComponent('5900'); /* 발전소 코드 */ //5100 하동
-            queryParams += '&' + encodeURIComponent('strHoki') + '=' + encodeURIComponent('1'); /* 호기 코드 */
+            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('30'); /* 한 페이지 결과 수 */
+            queryParams += '&' + encodeURIComponent('strSdate') + '=' + encodeURIComponent(startdate); /* 조회 시작월 */
+            queryParams += '&' + encodeURIComponent('strEdate') + '=' + encodeURIComponent(enddate); /* 조회 종료월(최대 1년) */
+            queryParams += '&' + encodeURIComponent('strOrgCd') + '=' + encodeURIComponent(powercode); /* 발전소 코드 */ //5100 하동
+            queryParams += '&' + encodeURIComponent('strHoki') + '=' + encodeURIComponent(ho); /* 호기 코드 */
+
+//  tmpUrl3 => 배출농도( 일별 )
+            // var queryParams = '?' + encodeURIComponent('ServiceKey') + '=22op4iVErXKCKm1jqNWSpzQ3Mo%2FoQYIIOquxrGwyyNSnC86o21TLuPaQGQ%2BH%2BLRT0hsvTo%2BG7UaPsBrMhmRZOg%3D%3D'; /* Service Key*/
+            // queryParams += '&' + encodeURIComponent('ServiceKey') + '=' + encodeURIComponent('22op4iVErXKCKm1jqNWSpzQ3Mo%2FoQYIIOquxrGwyyNSnC86o21TLuPaQGQ%2BH%2BLRT0hsvTo%2BG7UaPsBrMhmRZOg%3D%3D'); /* 공공데이터포털에서 받은 인증키 */
+            // queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지번호 */
+            // queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* 한 페이지 결과 수 */
+            // queryParams += '&' + encodeURIComponent('strSdate') + '=' + encodeURIComponent(startdate); /* 조회 시작월 */
+            // queryParams += '&' + encodeURIComponent('strEdate') + '=' + encodeURIComponent(enddate); /* 조회 종료월(최대 1년) */
+            // queryParams += '&' + encodeURIComponent('strOrgCd') + '=' + encodeURIComponent(powercode); /* 발전소코드 */
+            // queryParams += '&' + encodeURIComponent('strSnumPos') + '=' + encodeURIComponent(ho); /* 지역코드 */
+
 
             request({
                 url: url + queryParams,
@@ -60,44 +82,53 @@ router.get('/', function(req, res) {
             var parser = new xml2js.Parser();
 
             fs.readFile(__dirname + "/data.xml", function(err, data) {
-                console.log("what " + data);
+                console.log("here " + data);
 
                 parser.parseString(data, function(err, result) {
 
                     var list = [];
-                    console.log( result.response.header.length );
-                    for (let i = 0; i < result.response.header.length; i++) {
+                    var sumAvgair02 = 0;
+
+                    if( result.response.header ) {
+
+                        for (let i = 0; i < result.response.header.length; i++) {
+
+                        sumAvgair02 += Number(result.response.header[i].avgair02[0]);
 
                         let data = {
-                            orgNm : result.response.header[i].orgNm[0] ,
-                            date : result.response.header[i].ym[0] ,
+                            orgNm: result.response.header[i].orgNm[0],
+                            date: result.response.header[i].ym[0],
                             avgair01: result.response.header[i].avgair01[0],
                             avgair02: result.response.header[i].avgair02[0],
                             avgair03: result.response.header[i].avgair03[0]
                         }
                         list.push(data);
-                    }
+                        }
 
+                    }
                     connection.release();
-                    callback(null, list);
+                    callback(null, list, sumAvgair02);
                 });
             });
         },
 
-        function(list, callback) {
+        function(list, sumAvgair02, callback) {
 
-            console.log( "hehe " + list);
-            fs.writeFile(__dirname + "/yearData.txt", list, 'utf8', function(err) {
+            console.log("hehe " + list);
+            fs.writeFile(__dirname + "/yearData.js", list, 'utf8', function(err) {
                 if (err)
                     console.log("error : " + err);
                 else {
 
                     res.status(200).send({
                         status: "success",
-                        data: list,
-                        message: "successful get cityList"
+                        data: {
+                            sumAvgair02: sumAvgair02,
+                            datalist: list
+                        },
+                        message: "successful get publicdata"
                     });
-                    callback(null, "successful get cityList");
+                    callback(null, "successful get publicdata");
                 }
             });
         }
